9043abb0ac9ad14b5b7c15c23b478e102ab04778
GrippingGoose33 <grippinggoose33@hotmail.com>
2022-04-29 10:57:26 -0500

    first comite

---
+++ HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import './styles.js';
+
+function HomeScreen() {
+    return (
+        <View style={styles.container   }>
+            <Text>Home</Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
